6a7e1667dcf0eb1e489f35161a38990fb12c70ec
FELIPE AGUILAR <felipeaguilarwenig@uc.cl>
2023-02-10 01:11:09 -0300

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <h2 className='font-bold text-3xl items-center justify-center'>Engineering student @ UC | Chile</h2>
       </div>
       <div className='animate-land text-xl text-justify mx-60'>
-        Hey gamers! I'm an enthusiastic programmer interested in computer science, web development and physics, astronomy. I love exploring new technologies and finding creative solutions to complex problems. I'm excited to be a part of the constantly evolving world of technology and look forward to continuing to learn and grow as a programmer and web developer.
+        Hey gamers! I'm an enthusiastic programmer interested in computer science, web development, physics and astronomy. I love exploring new technologies and finding creative solutions to complex problems. I'm excited to be a part of the constantly evolving world of technology and look forward to continuing to learn and grow as a programmer and web developer.
       </div>
       <div>
         <div className='animate-land pt-12 pb-10'>
