301eabdb81eabb8d61002b254d0fd6d0a9f4a2f8
FELIPE AGUILAR <felipeaguilarwenig@uc.cl>
2023-02-09 23:50:57 -0300

    add: tons

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,33 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='animate-land p-20'>
+        <h1 className=" font-bold text-7xl font-mono">
+          Felipe Aguilar
+        </h1>
+        <h2 className='font-bold text-3xl items-center justify-center'>Engineering student @ UC | Chile</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='animate-land text-xl text-justify mx-60'>
+        Hey gamers! I'm an enthusiastic programmer interested in computer science, web development and physics and astronomy. I love exploring new technologies and finding creative solutions to complex problems. I'm excited to be a part of the constantly evolving world of technology and look forward to continuing to learn and grow as a programmer and web developer.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='animate-land p-20'>
+        <h1 className=" font-bold text-6xl font-mono">
+          Goals
+        </h1>
+      </div>
+      <div className='animate-land text-xl text-justify mx-60'>
+        (wip)
+      </div>
+      <div className='animate-land p-20'>
+        <h1 className=" font-bold text-6xl font-mono">
+          Projects
+        </h1>
+      </div>
+      <div className='animate-land text-xl text-justify mx-60'>
+        (wip)
+      </div>
     </div>
   )
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        land: 'text 1s ease',
+      },
+      keyframes: {
+        text: {
+          '0%': {
+            color: 'transparent',
+            transform: 'translateY(50%)',
+            },
+          '100%': {
+            
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
